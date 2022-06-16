b72133d0e854d29f5113f2f992e207b660fd7886
Joanne Imbisi <evizengwa@yahoo.com>
2022-06-15 23:38:42 -0700

    commit tree 2cc574995566df3460312b610f5b18e076b69b79

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record) {
+  const result = record.find( record => record.result === "W");
+    return (result ? result.year : undefined);
+}
+
+// superbowlWin = (record) => {
+//     const result = record.find( record => record.result === "W" );
+//     return !!result ? result.year : undefined;
+//   }
